019815262a2daf09ecd5b6f57b7ec3a7f2a01212
Larry <lbecker@gravitationalmarketing.com>
2022-05-27 08:19:42 -0400

    Code style changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from './App';
+import "@blueprintjs/core/lib/css/blueprint.css";
 
 ReactDOM.render(<App />, document.getElementById('app'));
