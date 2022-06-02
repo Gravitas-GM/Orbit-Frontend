c9810627d7a38913203e14f8655b98c9bc41d633
Larry <lbecker@gravitationalmarketing.com>
2022-06-02 08:25:29 -0400

    Create Nav Header components

---
+++ src/Components/NavHeader.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {Button, Intent, Menu, MenuDivider, MenuItem, Popover, Spinner} from '@blueprintjs/core';
+import './NavHeader.scss';
+
+interface IProps {
+	loading: boolean;
+}
+
+export const NavHeader: React.FC<IProps> = props => (
+	props.loading ? (
+		<Spinner intent={Intent.PRIMARY} />
+	) : (
+		<div id="nav-header">
+			<div style={{flex: 8}}>
+				Happy Orbit
+			</div>
+
+			<Popover>
+				<Button text={'Welcome'} rightIcon={'caret-down'} minimal={true} />
+
+				<Menu>
+					<MenuItem
+						text="Test Item"
+						icon="person"
+					/>
+
+					<MenuDivider />
+
+					<MenuItem
+						text="Log Out"
+						icon="log-out"
+					/>
+				</Menu>
+			</Popover>
+		</div>
+	)
+);
+
+NavHeader.displayName = 'NavHeader';
