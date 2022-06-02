59a1660f165549f8067f054a17b6afde3feb8b28
Larry <lbecker@gravitationalmarketing.com>
2022-06-02 08:44:46 -0400

    Create Home page skeleton

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const Home: React.FC = () => (
+	// TODO: create actual Home Page /Larry
+	<div>
+		Welcome to Orbit!
+	</div>
+);
+
+Home.displayName = 'Home';
