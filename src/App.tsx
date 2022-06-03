47d7ab985a08b729a608192d12c5d3ee55feea24
Larry <lbecker@gravitationalmarketing.com>
2022-06-03 14:58:19 -0400

    Implement login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import {Route, Router, Switch} from 'react-router';
+import {User} from './Api/Hub/Models/Users';
+import {Login} from './Components/Auth/Login';
 import {Layout} from './Components/Layout';
 import {history} from './history';
 
 interface IState {
 	loading: boolean;
+	user: User | null;
 }
 
 export class App extends React.PureComponent<{}, IState> {
 	public state: Readonly<IState> = {
 		loading: false,
+		user: null,
 	};
 
 	public render(): JSX.Element {
@@ -17,6 +21,10 @@ export class App extends React.PureComponent<{}, IState> {
 			<div id="app-root">
 				<Router history={history}>
 					<Switch>
+						<Route path="/login">
+							<Login onLoginSuccess={this.onUserChange} />
+						</Route>
+
 						<Route path="/">
 							<Layout loading={this.state.loading} />
 						</Route>
@@ -25,4 +33,11 @@ export class App extends React.PureComponent<{}, IState> {
 			</div>
 		);
 	}
+
+	private onUserChange = (newUser: User) => {
+		this.setState({
+			user: newUser,
+			loading: true,
+		});
+	};
 }
