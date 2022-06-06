502a4f773590521013410ecec8eeaae80ae67f06
Larry <lbecker@gravitationalmarketing.com>
2022-06-06 15:35:38 -0400

    Implement PrivateRoot

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {User} from './Api/Hub/Models/Users';
 import {Login} from './Components/Auth/Login';
 import {Layout} from './Components/Layout';
 import {history} from './history';
+import {PrivateRoute} from './PrivateRoute';
 import {UserContext} from './Session';
 
 interface IState {
@@ -27,9 +28,9 @@ export class App extends React.PureComponent<{}, IState> {
 								<Login onLoginSuccess={this.onUserChange} />
 							</Route>
 
-							<Route path="/">
+							<PrivateRoute path="/">
 								<Layout loading={this.state.loading} />
-							</Route>
+							</PrivateRoute>
 						</Switch>
 					</Router>
 				</UserContext.Provider>
