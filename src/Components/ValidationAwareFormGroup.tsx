08c345704d5060afbc4e077785495fdd8419d628
Larry <lbecker@gravitationalmarketing.com>
2022-06-09 09:47:10 -0400

    Create ValidationAwareFormGroup

---
+++ src/Components/ValidationAwareFormGroup.tsx
@@ -0,0 +1,32 @@
+import {FormGroup as BPFormGroup, IFormGroupProps, Intent} from '@blueprintjs/core';
+import * as React from 'react';
+import {ValidationFailures} from '../Api/errors';
+
+interface IProps extends IFormGroupProps {
+	labelFor: string;
+	failures: ValidationFailures | null;
+	exactMatch?: boolean;
+}
+
+export const ValidationAwareFormGroup: React.FC<IProps> = ({failures, exactMatch, ...props}) => {
+	if (failures) {
+		if (props.labelFor in failures) {
+			props.helperText = failures[props.labelFor].message;
+			props.intent = Intent.DANGER;
+		} else if (!exactMatch) {
+			for (let key in failures) {
+				if (!failures.hasOwnProperty(key))
+					continue;
+
+				if (key.indexOf(props.labelFor) === 0) {
+					props.helperText = failures[key].message;
+					props.intent = Intent.DANGER;
+				}
+			}
+		}
+	}
+
+	return <BPFormGroup {...props} />;
+};
+
+ValidationAwareFormGroup.displayName = `ValidationAwareFormGroup(${BPFormGroup.displayName})`;
