111e97aa21743442f2fbf17eb08f495e2c497ee5
Larry <lbecker@gravitationalmarketing.com>
2022-06-15 10:14:15 -0400

    Implement users and sources list pages

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -4,6 +4,8 @@ import {Route, Switch} from 'react-router';
 import {Home} from './Home';
 import {NavHeader} from './NavHeader';
 import {PageNotFound} from './PageNotFound';
+import {SourcesList} from './Pages/Sources';
+import {UsersList} from './Pages/Users';
 
 interface IProps {
 	loading: boolean;
@@ -21,6 +23,10 @@ export const Layout: React.FC<IProps> = props => (
 			<div className="main-frame">
 				<Switch>
 					<Route path="/" component={Home} exact={true} />
+
+					<Route path="/users" component={UsersList} exact={true} />
+
+					<Route path="/sources" component={SourcesList} exact={true} />
 
 					<Route component={PageNotFound} />
 				</Switch>
