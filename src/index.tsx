12fedfdd7c476bea0f917179e1c957ef9abfe70a
Tyler Lartonoix <tyler@lartonoix.com>
2022-06-21 15:27:17 -0400

    Add dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import './index.scss';
+import 'dotenv/config';
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 root.render(<App />);
