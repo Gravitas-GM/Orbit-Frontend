145d5fa443519394457f0ef324d05376ebd2ebc3
Tyler Lartonoix <tyler@lartonoix.com>
2022-06-21 15:30:27 -0400

    Remove unnecessary cast

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import 'dotenv/config';
 
-const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('app')!);
 root.render(<App />);
