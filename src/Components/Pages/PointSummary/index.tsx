6001c94b19e9c01fef679545f1b54ca9a51dc57c
Larry <lbecker@gravitationalmarketing.com>
2022-06-29 13:05:58 -0400

    Implement point summary page

--- src/Components/Pages/PointSummary/index.tsx
+++ src/Components/Pages/PointSummary/index.tsx
@@ -1,6 +1,6 @@
-import {H2, H4, HTMLTable} from '@blueprintjs/core';
+import {H2, HTMLTable} from '@blueprintjs/core';
 import * as React from 'react';
-import {PointsModel, UserPoints} from '../../../Api/Point-Tracking/Models/Points';
+import {PointsModel, UserPointsSummary} from '../../../Api/Point-Tracking/Models/Points';
 import {UserContext} from '../../../Session';
 import * as toaster from '../../../Toaster';
 import {FrameLoadingSpinner} from '../../FrameLoadingSpinner';
@@ -8,7 +8,7 @@ import {classNames} from '../../Utility/dom';
 import {formatNumber, ucwords} from '../../Utility/string';
 
 interface IState {
-	userPoints: UserPoints[];
+	userPoints: UserPointsSummary[];
 	loading: boolean;
 }
 
@@ -22,16 +22,16 @@ export class PointSummary extends React.PureComponent<{}, IState> {
 	};
 
 	public async componentDidMount() {
-		let userPoints: UserPoints[] = [];
+		let userPoints: UserPointsSummary[] = [];
 
 		try {
-			userPoints = await PointsModel.getAll(this.context!.account.id).then(response => response.data);
+			userPoints = await PointsModel.getAllSummary(this.context!.account.id).then(response => response.data);
 		} catch (_) {
 			toaster.showUnhandledErrorMessage();
 		}
 
 		this.setState({
-			userPoints: userPoints.sort((a, b) => a.user_name.localeCompare(b.user_name)),
+			userPoints: userPoints.sort((a, b) => a.points - b.points),
 			loading: false,
 		});
 	}
@@ -44,32 +44,23 @@ export class PointSummary extends React.PureComponent<{}, IState> {
 			<>
 				<H2>Point Summary</H2>
 
-				{this.state.userPoints.map(item => (
-					<>
-						<H4>{item.user_name}</H4>
+				<HTMLTable className={classNames('bp4-html-table-striped')}>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Total Points</th>
+						</tr>
+					</thead>
 
-						<HTMLTable className={classNames('bp4-html-table-striped')}>
-							<thead>
-								<tr key={`user-points-${item.user_id}`}>
-									<th>Source</th>
-									<th>Point Value</th>
-									<th>Timestamp</th>
-									<th>Description</th>
-								</tr>
-							</thead>
-							<tbody>
-								{item.points.map(points => (
-									<tr key={`point-item-${points.id}`}>
-										<td>{ucwords(points.source)}</td>
-										<td>{formatNumber(points.point_value)}</td>
-										<td>{points.timestamp.toDateString()}</td>
-										<td>{points.description}</td>
-									</tr>
-								))}
-							</tbody>
-						</HTMLTable>
-					</>
-				))}
+					<tbody>
+						{this.state.userPoints.map(item => (
+							<tr key={`point-summary-${item.user_id}`}>
+								<td>{ucwords(item.user_name)}</td>
+								<td>{formatNumber(item.points)}</td>
+							</tr>
+						))}
+					</tbody>
+				</HTMLTable>
 			</>
 		);
 	}
