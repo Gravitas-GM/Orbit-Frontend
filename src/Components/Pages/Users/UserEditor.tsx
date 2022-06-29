b62d1fdd82619c6158d3377c7a313f86169a0204
Larry <lbecker@gravitationalmarketing.com>
2022-06-29 14:28:28 -0400

    Fix padding on User Editor

--- src/Components/Pages/Users/UserEditor.tsx
+++ src/Components/Pages/Users/UserEditor.tsx
@@ -150,7 +150,7 @@ export class UserEditor extends React.PureComponent<RouteComponentProps<IRoutePr
 					</tbody>
 				</HTMLTable>
 
-				<div className={classNames('settings-title-container')}>
+				<div className={classNames('settings-title-container')} style={{paddingTop: 25}}>
 					<H2>Points</H2>
 
 					<Button
