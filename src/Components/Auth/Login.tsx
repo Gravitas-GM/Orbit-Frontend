de341934592fc9f2f2bdbc6a5a49c424aaa79e86
Tyler Lartonoix <tyler@lartonoix.com>
2022-07-25 15:39:18 -0400

    Replace deprecated uses of KeyboardEvent.keyCode

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -78,9 +78,9 @@ class Login extends React.PureComponent<IProps, IState> {
 		);
 	}
 
-	private onFormKeyDown = (event: React.KeyboardEvent) => {
-		if (event.keyCode === 13)
-			this.onLoginSubmit(event);
+	private onFormKeyDown = async (event: React.KeyboardEvent) => {
+		if (event.code === 'KeyEnter')
+			await this.onLoginSubmit(event);
 	};
 
 	private onEmailAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => this.setState({
