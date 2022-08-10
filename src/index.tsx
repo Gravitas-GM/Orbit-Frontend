e2e175e8b3b5d6479ef3cff2df75f3907b75a7ac
Tyler Lartonoix <tyler@lartonoix.com>
2022-08-10 14:39:27 -0400

    Only use .env for non-prod builds

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production')
+	dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('app')!);
 root.render(<App />);
