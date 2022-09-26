10ef5903b7d395eea1e3e1a6ce6ec892b664b48c
Larry <lbecker@gravitationalmarketing.com>
2022-09-26 09:55:06 -0400

    Update Board model and endpoints

--- src/Api/Game-Catalog/Models/Boards.ts
+++ src/Api/Game-Catalog/Models/Boards.ts
@@ -1,31 +1,12 @@
-import {gameCatalogClient, Id, Projectable, Projection, QueryDocument} from '../..';
+import {gameCatalogClient, Id} from '../..';
 import {Stage} from './Stages';
 
 export interface BoardEndpoints {
-	'/boards': {
-		PUT: {
-			query: Projectable;
-			body: BoardCreatePayload;
-			response: Board;
-		};
-	};
-
 	'/boards/:id': {
 		GET: {
 			params: Id;
 			response: Board;
 		};
-
-		PATCH: {
-			params: Id;
-			body: BoardUpdatePayload;
-			response: Board;
-		};
-
-		DELETE: {
-			params: Id;
-			response: void;
-		}
 	};
 }
 
@@ -34,41 +15,12 @@ export interface Board {
 	name: string;
 	game: number;
 	sequence: number;
-	image: string;
+	imageUrl: string;
 	stages: Stage[];
 }
 
-export type BoardCreatePayload = Omit<Board, 'id' | 'stages'>;
-
-export type BoardUpdatePayload = Partial<Omit<Board, 'id' | 'stages'>>;
-
 export class BoardModel {
-	public static list(projection?: Projection, query?: QueryDocument) {
-		return gameCatalogClient.get('/boards', {
-			params: {
-				p: projection,
-				q: query,
-			},
-		});
-	}
-
-	public static create(payload: BoardCreatePayload, projection?: Projection) {
-		return gameCatalogClient.put('/boards', payload, {
-			params: {
-				p: projection,
-			},
-		});
-	}
-
 	public static read(board: Id) {
 		return gameCatalogClient.get<'/boards/:id'>(`/boards/${board}`);
-	}
-
-	public static update(board: Id, payload: BoardUpdatePayload) {
-		return gameCatalogClient.patch<'/boards/:id'>(`/boards/${board}`, payload);
-	}
-
-	public static delete(board: Id) {
-		return gameCatalogClient.delete<'/boards/:id'>(`/boards/${board}`);
 	}
 }
