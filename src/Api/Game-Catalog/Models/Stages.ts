4360f904070f75cd35e06fd6c4abab3699661623
Larry <lbecker@gravitationalmarketing.com>
2022-09-26 09:55:26 -0400

    Update Stages model and endpoints

--- src/Api/Game-Catalog/Models/Stages.ts
+++ src/Api/Game-Catalog/Models/Stages.ts
@@ -1,31 +1,19 @@
-import {gameCatalogClient, Id, Projectable, Projection, QueryDocument} from '../..';
+import {gameCatalogClient, Id} from '../..';
 
 export interface StageEndpoints {
-	'/stages': {
-		PUT: {
-			query: Projectable;
-			body: StageCreatePayload;
-			response: Stage;
-		};
-	};
-
 	'/stages/:id': {
 		GET: {
 			params: Id;
 			response: Stage;
 		};
-
-		PATCH: {
-			params: Id;
-			body: StageUpdatePayload;
-			response: Stage;
-		};
-
-		DELETE: {
-			params: Id;
-			response: void;
-		}
 	};
+}
+
+export interface BoardRegion {
+	x: number;
+	y: number;
+	width: number;
+	height: number;
 }
 
 export interface Stage {
@@ -33,43 +21,11 @@ export interface Stage {
 	name: string;
 	board: number;
 	requiredPoints: number;
-	positionX: number;
-	positionY: number;
-	positionWidth: number;
-	positionHeight: number;
+	boardRegion: BoardRegion;
 }
 
-export type StageCreatePayload = Omit<Stage, 'id'>;
-
-export type StageUpdatePayload = Partial<Omit<Stage, 'id'>>;
-
 export class StageModel {
-	public static list(projection?: Projection, query?: QueryDocument) {
-		return gameCatalogClient.get('/stages', {
-			params: {
-				p: projection,
-				q: query,
-			},
-		});
-	}
-
-	public static create(payload: StageCreatePayload, projection?: Projection) {
-		return gameCatalogClient.put('/stages', payload, {
-			params: {
-				p: projection,
-			},
-		});
-	}
-
 	public static read(stage: Id) {
 		return gameCatalogClient.get<'/stages/:id'>(`/stages/${stage}`);
-	}
-
-	public static update(stage: Id, payload: StageUpdatePayload) {
-		return gameCatalogClient.patch<'/stages/:id'>(`/stages/${stage}`, payload);
-	}
-
-	public static delete(stage: Id) {
-		return gameCatalogClient.delete<'/stages/:id'>(`/stages/${stage}`);
 	}
 }
