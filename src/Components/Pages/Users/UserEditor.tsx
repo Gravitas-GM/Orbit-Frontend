fb46d1048457d0f3c3c343f06fe083a5125f0615
Larry <lbecker@gravitationalmarketing.com>
2022-10-10 16:56:30 -0400

    Replace mdash with —

--- src/Components/Pages/Users/UserEditor.tsx
+++ src/Components/Pages/Users/UserEditor.tsx
@@ -159,7 +159,7 @@ export class UserEditor extends React.PureComponent<RouteComponentProps<IRoutePr
 								<td>{ucwords(item.source)}</td>
 								<td>{formatNumber(item.point_value)}</td>
 								<td>{new Date(item.timestamp).toLocaleString()}</td>
-								<td>{item.description ?? <>&mdash;</>}</td>
+								<td>{item.description ?? <>—</>}</td>
 								<td style={{textAlign: 'center'}}>
 									<Button
 										icon="delete"
