aba60d6a64d6e90104054db0ab4e684bf49f579b
Eduardo Iotti <ddz.iotti@gmail.com>
2022-10-25 10:40:50 -0300

    Create game card component

---
+++ src/Components/Game/Card/index.tsx
@@ -0,0 +1,23 @@
+import {Icon, IconName} from '@blueprintjs/core';
+import './Card.scss'
+
+interface IProps {
+	title: string;
+	icon: IconName;
+	children: React.ReactNode;
+}
+
+export const GameCard: React.FC<IProps> = props => {
+	return (
+		<div className="gm-card">
+			<header className="gm-card-header">
+				<Icon icon={props.icon} style={{marginRight: '0.5rem'}} />
+				{props.title}
+			</header>
+
+			<div className="gm-card-content">
+				{props.children}
+			</div>
+		</div>
+	);
+};
