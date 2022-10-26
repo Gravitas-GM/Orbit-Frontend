a68892621ac21a04933954fd06000a07947646c2
Tyler Lartonoix <tyler@lartonoix.com>
2022-10-26 18:52:07 -0400

    Clean up card component

--- src/Components/Game/Card/index.tsx
+++ src/Components/Game/Card/index.tsx
@@ -1,32 +1,32 @@
-import React, {useState, useCallback} from 'react';
-import {Icon, IconName} from '@blueprintjs/core';
+import React, { useState, useCallback } from 'react';
+import { Icon, IconName } from '@blueprintjs/core';
 import './Card.scss';
+import { classNames } from '../../Utility/dom';
+import { Classes } from '../../../classes';
 
 interface IProps {
 	title: string;
 	icon: IconName;
-	children: React.ReactNode;
 	fill?: boolean;
+	children?: React.ReactNode;
 }
 
-export const GameCard: React.FC<IProps> = ({fill, title, icon, children}) => {
+export const GameCard: React.FC<IProps> = ({ fill, title, icon, children }) => {
 	const [open, setOpen] = useState(true);
 
-	const onCollapse = useCallback(() => {
-		setOpen(currentOpen => !currentOpen);
+	const onCollapseToggle = useCallback((e: React.MouseEvent<HTMLDetailsElement>) => {
+		e.preventDefault();
+		setOpen(open => !open);
 	}, []);
 
 	return (
-		<details className={`gm-card ${fill && open ? 'fill' : ''}`} onClick={e => e.preventDefault()} open={open}>
-			<summary
-			className={`gm-card-header ${!open ? 'collapsed' : ''}`}
-			onClick={() => onCollapse()}
-			>
-				<Icon icon={icon} style={{marginRight: '0.5rem'}} />
+		<details className={classNames(Classes.CARD, !open && 'collapsed', fill && 'fill')} open={open}>
+			<summary className={Classes.CARD_HEADER} onClick={onCollapseToggle}>
+				<Icon icon={icon} style={{ marginRight: '0.5rem' }} />
 				{title}
 			</summary>
 
-			<div className="gm-card-content">{children}</div>
+			<div className={Classes.CARD_BODY}>{children}</div>
 		</details>
 	);
 };
