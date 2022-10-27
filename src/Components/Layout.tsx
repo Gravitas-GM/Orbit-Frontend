63419c1383a074434a64d5d29f3845fc99118944
Larry <lbecker@gravitationalmarketing.com>
2022-10-27 12:30:10 -0400

    Add Game route to layout

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -5,6 +5,7 @@ import {Permission, PermissionContext} from '../Permission';
 import {Home} from './Home';
 import {NavHeader} from './NavHeader';
 import {PageNotFound} from './PageNotFound';
+import {GameBoard} from './Pages/Game';
 import {PointSummary} from './Pages/PointSummary';
 import {SourcesList} from './Pages/Sources';
 import {UsersList} from './Pages/Users';
@@ -30,6 +31,8 @@ export const Layout: React.FC<IProps> = props => (
 							<Route path="/" component={Home} exact={true} />
 
 							<Route path="/point-summary" component={PointSummary} exact={true} />
+
+							<Route path="/game" component={GameBoard} />
 
 							{isGranted(Permission.ADMIN) && [
 								<Route path="/users" key="/users" component={UsersList} exact={true} />,
