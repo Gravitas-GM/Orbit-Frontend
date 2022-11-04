54eb55cd7e38a534e23a6d96a5ef7c8c5eef0eb3
Eduardo Iotti <ddz.iotti@gmail.com>
2022-11-04 11:48:21 -0300

    Create log history card component

---
+++ src/Components/Pages/Game/Sidebar/LogHistoryCard/index.tsx
@@ -0,0 +1,121 @@
+import React, {useMemo, useState, useContext, useCallback} from 'react';
+import {Button, Icon} from '@blueprintjs/core';
+import {ObjectId} from '../../../../../Api/Point-Tracking';
+import {NonIdealState} from '../../../../NonIdealState';
+import {GameCard} from '../GameCard/GameCard';
+import {formatDate} from '../../../../Utility/date';
+import {IconSize} from '../../../../../IconSize';
+import {UserContext} from '../../../../../Session';
+import {HistoryItem, HistoryModel} from '../../../../../Api/Game-State/Models/History';
+import * as toaster from '../../../../../Toaster';
+import {Classes} from '../../../../../classes';
+import './LogHistoryCard.scss';
+
+interface IProps {
+	logItems: HistoryItem[];
+}
+
+type DateGroup = {
+	[key: string]: HistoryItem[];
+};
+
+export const LogHistoryCard: React.FC<IProps> = ({logItems}) => {
+	const [processing, setIsProcessing] = useState(false);
+	const [currentItems, setCurrentItems] = useState(logItems);
+	const User = useContext(UserContext);
+
+	const groupedDates = useMemo(() => {
+		return currentItems.reduce((dates: DateGroup, item) => {
+			const {timestamp} = item;
+			const formattedDate = formatDate(timestamp);
+
+			dates[formattedDate] = dates[formattedDate] ?? [];
+			dates[formattedDate].push(item);
+
+			return dates;
+		}, {});
+	}, [currentItems]);
+
+	const currentDateFormatted = useMemo(() => formatDate(new Date()), []);
+
+	const onLoadMoreClick = useCallback(async (lastItemId: ObjectId | undefined) => {
+		if (!User)
+			return;
+
+		if (lastItemId === undefined)
+			return;
+
+		else {
+			setIsProcessing(true);
+
+			await HistoryModel.getAfter(User.account.id, lastItemId)
+				.then(({data}) => setCurrentItems(data))
+				.catch(_ => {
+					toaster.showUnhandledErrorMessage();
+				});
+
+			setIsProcessing(false);
+		}
+
+	},[]);
+
+	const onRefreshClick = useCallback(async () => {
+		if (!User)
+			return;
+
+		else {
+			setIsProcessing(true);
+
+			await HistoryModel.get(User.account.id)
+				.then(({data}) => setCurrentItems(data))
+				.catch(_ => {
+					toaster.showUnhandledErrorMessage();
+				});
+
+			setIsProcessing(false);
+		}
+
+	},[]);
+
+	return (
+		<GameCard title="Log History" icon="history">
+			{currentItems.length === 0 ? (
+				<NonIdealState
+					hideIcon
+					title="There isn't any history yet."
+					action={<Button icon="refresh" text="Refresh history" onClick={onRefreshClick} loading={processing} />}
+				/>
+			) : (
+				<>
+					{Object.keys(groupedDates).map(date => {
+						return (
+							<ul key={date} className={Classes.LOG_HISTORY_CARD}>
+								{currentDateFormatted !== date && <span className={Classes.ITEM_DATE}>{date}</span>}
+
+								{groupedDates[date].map(logItem => (
+									<LogItem key={logItem.id.$oid}>{logItem.content}</LogItem>
+								))}
+							</ul>
+						);
+					})}
+
+					<div className={Classes.CARD_BUTTON_WRAPPER}>
+						<Button onClick={() => onLoadMoreClick(currentItems.at(-1)?.id)} loading={processing}>
+							Load More...
+						</Button>
+					</div>
+				</>
+			)}
+		</GameCard>
+	);
+};
+
+const LogItem: React.FC<{children: React.ReactNode}> = ({children}) => {
+	return (
+		<li>
+			<Icon icon="direction-right" size={IconSize.SMALL} />
+
+			<span className={Classes.ITEM_CONTENT}>{children}</span>
+		</li>
+	);
+};
--- src/classes.ts
+++ src/classes.ts
@@ -2,4 +2,8 @@ export const Classes = {
 	CARD: 'gm-card',
 	CARD_HEADER: 'card-header',
 	CARD_BODY: 'card-content',
+	LOG_HISTORY_CARD: 'gm-log-history-card',
+	ITEM_DATE: 'item-date',
+	ITEM_CONTENT: 'item-content',
+	CARD_BUTTON_WRAPPER: 'button-wrapper'
 };
