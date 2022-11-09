c933c68e38303abbecfeb04734ee277f806a0b0f
Eduardo Iotti <ddz.iotti@gmail.com>
2022-11-09 14:54:33 -0300

    fix: Set hide icon prop to optional

--- src/Components/NonIdealState.tsx
+++ src/Components/NonIdealState.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 type BaseProps = Omit<NonIdealStateProps, 'title'>;
 type Props = BaseProps & {
 	title: React.ReactNode;
-	hideIcon: boolean;
+	hideIcon?: boolean;
 };
 
 export const NonIdealState: React.FC<Props> = ({icon, hideIcon = false, ...props}) =>	<Base {...props} icon={hideIcon ? null : (icon ?? 'wind')}/>;
