aac1d9917e8e8765d7bd23f81526ee42d777f592
Eduardo Iotti <ddz.iotti@gmail.com>
2022-11-15 17:05:52 -0300

    Refactor format date util

--- src/Components/Utility/date.ts
+++ src/Components/Utility/date.ts
@@ -9,21 +9,10 @@ export function parseApiTimestamp(input: any): Date {
 	throw new Error(`Cannot convert input to Date`);
 }
 
-export function formatDate(input: any) {
-	if (typeof input === 'string')
-		return new Intl.DateTimeFormat('default', {
-			month: 'long',
-			day: 'numeric',
-			year: 'numeric',
-		}).format(parseApiTimestamp(input));
-	else if (input instanceof Date)
-		return new Intl.DateTimeFormat('default', {
-			month: 'long',
-			day: 'numeric',
-			year: 'numeric',
-		}).format(input);
-
-	console.error('Cannot format provided input: ', input);
-
-	throw new Error(`Cannot format provided input`);
+export function formatDate(input: Date) {
+	return new Intl.DateTimeFormat('default', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
+	}).format(input);
 }
