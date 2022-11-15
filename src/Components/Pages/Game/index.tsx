dd2f1d489cd5d8eefe4bcfd52464e5d3240db26c
Eduardo Iotti <ddz.iotti@gmail.com>
2022-11-15 17:24:07 -0300

    Refactor log history calls at game page

--- src/Components/Pages/Game/index.tsx
+++ src/Components/Pages/Game/index.tsx
@@ -14,6 +14,7 @@ interface IState {
 	board: Board | null;
 	gameState: GameState | null;
 	history: HistoryItem[] | null;
+	loadingHistory: boolean;
 	loading: boolean;
 	redirect: boolean;
 }
@@ -25,7 +26,8 @@ export class GameBoardPage extends React.PureComponent<{}, IState> {
 	public state: Readonly<IState> = {
 		board: null,
 		gameState: null,
-		history: null,
+		history:  [],
+		loadingHistory: false,
 		loading: true,
 		redirect: false,
 	};
@@ -59,7 +61,7 @@ export class GameBoardPage extends React.PureComponent<{}, IState> {
 			return;
 		}
 
-		const history = await this.fetchLogHistory();
+		const history = await this.fetchHistory();
 
 		this.setState({
 			board,
@@ -78,26 +80,58 @@ export class GameBoardPage extends React.PureComponent<{}, IState> {
 		return (
 			<div style={{display: 'grid', gridTemplateColumns: '5fr 2fr'}}>
 				<H1>Game Board</H1>
+
 				<Sidebar>
-					<LogHistoryCard logItems={this.state.history} />
+					<LogHistoryCard
+						processing={this.state.loadingHistory}
+						history={this.state.history}
+						refresh={this.loadHistory}
+						loadMore={this.loadMoreHistory}
+					/>
 				</Sidebar>
 			</div>
 		);
 	}
 
 
-	private async fetchLogHistory() {
-		let history: HistoryItem[] | null;
-
+	private async fetchHistory() {
 		try {
-			history = await HistoryModel.get(this.context!.id).then(response => response.data);
+			return await HistoryModel.get(this.context!.id).then(response => response.data);
 		} catch (_) {
-
 			toaster.showUnhandledErrorMessage();
 
-			history = null;
+			return null;
 		}
+	}
 
-		return history;
+	private loadHistory = async () => {
+		this.setState({loadingHistory: true});
+
+		const history = await this.fetchHistory();
+
+		this.setState({history, loadingHistory: false});
+	}
+
+	private loadMoreHistory = async () => {
+		let items: HistoryItem[] = [];
+
+		this.setState({
+			loadingHistory: true
+		});
+
+		try {
+			items = await HistoryModel.getAfter(this.context!.id, this.state.history!.at(-1)!.id).then(response => response.data);
+
+			this.setState(({history}) => {
+				if (history !== null)
+					return { history: [...history, ...items], loadingHistory: false };
+				else
+					return { history: items, loadingHistory: false };
+			});
+		} catch (_) {
+			toaster.showUnhandledErrorMessage();
+
+			this.setState({ loadingHistory: false });
+		}
 	}
 }
