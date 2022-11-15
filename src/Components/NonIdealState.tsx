cb96819447a70ab6d37cbb2d63c7adf51486c7c1
Eduardo Iotti <ddz.iotti@gmail.com>
2022-11-15 17:05:27 -0300

    Refactor hide icon feature

--- src/Components/NonIdealState.tsx
+++ src/Components/NonIdealState.tsx
@@ -1,10 +1,20 @@
-import {NonIdealState as Base, NonIdealStateProps} from '@blueprintjs/core';
-import React from 'react';
+import { NonIdealState as Base, NonIdealStateProps, Icon as BPIcon } from "@blueprintjs/core";
+import { IconSize } from "../IconSize";
+import React from "react";
 
-type BaseProps = Omit<NonIdealStateProps, 'title'>;
+type BaseProps = Omit<NonIdealStateProps, "title">;
 type Props = BaseProps & {
 	title: React.ReactNode;
-	hideIcon?: boolean;
 };
 
-export const NonIdealState: React.FC<Props> = ({icon, hideIcon = false, ...props}) =>	<Base {...props} icon={hideIcon ? null : (icon ?? 'wind')}/>;
+export const NonIdealState: React.FC<Props> = ({ icon = false, ...props }) => (
+	<Base {...props} icon={<Icon icon={icon} />} />
+);
+
+const Icon: React.FC<Pick<NonIdealStateProps, "icon">> = ({ icon }) => {
+	if (typeof icon === "string" || typeof icon === "undefined")
+		return <BPIcon size={IconSize.LARGE} icon={icon ?? "wind"} />;
+
+	else
+		return null;
+};
