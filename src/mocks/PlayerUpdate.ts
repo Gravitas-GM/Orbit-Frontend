93aa46b3e5f786fef3d924d727afbfff5f99109a
Eduardo Iotti <ddz.iotti@gmail.com>
2022-12-15 15:33:48 -0300

    Add mocks for api payloads

---
+++ src/mocks/PlayerUpdate.ts
@@ -0,0 +1,35 @@
+import { PlayerUpdate, UpdateResultType } from "../Api/Game-State/Models/Games";
+import { playersMock } from "./Player";
+export const playerUpdateMock: PlayerUpdate[] = [
+	{
+		type: UpdateResultType.CREATED,
+		player: playersMock[0],
+		history_item: {
+			id: { $oid: 'id' },
+			account_id: 12,
+			timestamp: new Date(),
+			content: 'Moved to Colorado',
+		},
+	},
+	{
+		type: UpdateResultType.CHANGED,
+		player: playersMock[1],
+		new_point_total: 110,
+	},
+	{
+		type: UpdateResultType.MOVED,
+		player: playersMock[2],
+		new_point_total: 120,
+		new_stage_index: 2,
+		history_item: {
+			id: { $oid: 'id' },
+			account_id: 1,
+			timestamp: new Date(),
+			content: 'Moved to Los Angeles',
+		},
+	},
+	{
+		type: UpdateResultType.DELETED,
+		player_id: playersMock[3].hub_id,
+	},
+]
