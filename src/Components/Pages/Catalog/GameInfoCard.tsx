4a0982a8217ece6cbfb01adf18afd3e9d86bbb58
Larry <lbecker@gravitationalmarketing.com>
2023-01-03 09:15:34 -0500

    Create Game Info Card

---
+++ src/Components/Pages/Catalog/GameInfoCard.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Card, H2 } from '@blueprintjs/core';
+import { Game } from '../../../Api/Game-Catalog/Models/Games';
+import { ucwords } from '../../Utility/string';
+import './CatalogInfoCard.scss';
+
+interface IProps {
+	game: Game;
+	onClick: () => void;
+}
+
+export const GameInfoCard: React.FC<IProps> = ({ game, onClick }) => {
+	return (
+		<Card
+			className="catalog-info-card"
+			key={game.id}
+			interactive={true}
+			onClick={onClick}
+		>
+			<div style={{ display: 'flex' }}>
+				<img
+					src={game.thumbnailUrl ?? 'https://i.imgur.com/6Y1ocrb.png'}
+					alt={`${game.name} image`}
+					width="150"
+				/>
+
+				<div style={{ display: 'flex', flexDirection: 'column', paddingLeft: 20 }}>
+					<H2>{ucwords(game.name)}</H2>
+
+					<span>{game.description}</span>
+				</div>
+			</div>
+		</Card>
+	);
+};
+
+GameInfoCard.displayName = 'GameInfoCard';
