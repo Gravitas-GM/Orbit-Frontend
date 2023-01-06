8102a5258d36b452cf87ae1009fc2b8e60ed330d
Eduardo Iotti <ddz.iotti@gmail.com>
2023-01-06 17:52:01 -0300

    Simplify dialog confirmation

--- src/Components/Pages/Catalog/StartGameDialog.tsx
+++ src/Components/Pages/Catalog/StartGameDialog.tsx
@@ -11,22 +11,14 @@ interface IProps {
 }
 
 export const StartGameDialog: React.FC<IProps> = ({ isOpen, game, processing, onConfirm, onCancel }) => {
-	let [confirmText, setConfirmText] = React.useState('');
 
 	let onCancelCallback = React.useCallback(() => {
-		setConfirmText('');
 		onCancel();
-	}, [onCancel, setConfirmText]);
+	}, [onCancel]);
 
 	let onConfirmCallback = React.useCallback(() => {
-		setConfirmText('');
 		onConfirm();
-	}, [onConfirm, setConfirmText]);
-
-	let onConfirmTextChange = React.useCallback(
-		(event: React.ChangeEvent<HTMLInputElement>) => setConfirmText(event.currentTarget.value),
-		[setConfirmText],
-	);
+	}, [onConfirm]);
 
 	return (
 		<Dialog
@@ -35,15 +27,10 @@ export const StartGameDialog: React.FC<IProps> = ({ isOpen, game, processing, on
 			onClose={onCancelCallback}
 		>
 			<div className={Classes.DIALOG_BODY}>
-				<p>
-					Would you like to start {game.name}? This will reset all points for all users and start a new game.
+				<p style={{ lineHeight: '175%' }}>
+					Would you like to start {game.name}?<br />
+					This will reset all points for all users and start a new game.
 				</p>
-
-				<p>
-					To confirm, please type "{game.name}" in the box below, then click "Confirm."
-				</p>
-
-				<InputGroup value={confirmText} onChange={onConfirmTextChange} autoFocus={true} />
 			</div>
 
 			<div className={Classes.DIALOG_FOOTER}>
@@ -54,7 +41,6 @@ export const StartGameDialog: React.FC<IProps> = ({ isOpen, game, processing, on
 						text="Start Game"
 						intent={Intent.PRIMARY}
 						onClick={onConfirmCallback}
-						disabled={game.name !== confirmText}
 						loading={processing}
 					/>
 				</div>
