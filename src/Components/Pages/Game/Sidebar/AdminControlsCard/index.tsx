bf3290cfe174b5e9815d31f40076962c4d3d91a1
Eduardo Iotti <ddz.iotti@gmail.com>
2023-01-17 14:48:17 -0300

    Cleanup on Admin Controls Card component

--- src/Components/Pages/Game/Sidebar/AdminControlsCard/index.tsx
+++ src/Components/Pages/Game/Sidebar/AdminControlsCard/index.tsx
@@ -5,60 +5,28 @@ import { GameCard } from '../GameCard/GameCard';
 import { UpdatePreviewDialog } from './UpdatePreviewDialog/';
 import { Board } from '../../../../../Api/Game-Catalog/Models/Boards';
 
-/*
-Uncomment after feature merge
-*/
-
-// new game and next board
 import { ConfirmNextBoardDialog } from './ConfirmNextBoardDialog';
 import { NewGameDialog } from './NewGameDialog';
-
-
 
 interface IProps {
 	gameState: GameState;
 	board: Board;
-
-	// new game and next board
 	goToNextBoard: () => Promise<void>;
 	startNewGame: (gameId: GameStartPayload) => Promise<void>;
 }
 
 export const AdminControlsCard: React.FC<IProps> = ({ gameState, board, goToNextBoard, startNewGame }) => {
-
-	//  do we really need this?
-	const [processing, setIsProcessing] = useState({ preview: false, nextBoard: false, newGame: false });
-
 	const [showUpdatePreviewDialog, setShowUpdatePreviewDialog] = useState(false);
 
-
 	const onPreviewClick = useCallback(async () => {
-		setIsProcessing({
-			newGame: false,
-			nextBoard: false,
-			preview: true
-		});
-
 		setShowUpdatePreviewDialog(true);
 	}, []);
 
 	const closePreviewDialog = useCallback(async () => {
 		setShowUpdatePreviewDialog(false);
-
-		setIsProcessing({
-			newGame: false,
-			nextBoard: false,
-			preview: false
-		});
 	}, []);
 
-	/*
-
-	Uncomment after feature merge
-	*/
-	// next board
 	const [showConfirmNextBoardDialog, setShowConfirmNextBoardDialog] = useState(false);
-
 	const closeNextBoardDialog = useCallback(() => setShowConfirmNextBoardDialog(false), []);
 
 	const confirmNextBoard = useCallback(() => {
@@ -68,20 +36,12 @@ export const AdminControlsCard: React.FC<IProps> = ({ gameState, board, goToNext
 	const onConfirmNextBoard = useCallback(async () => {
 		setShowConfirmNextBoardDialog(false);
 
-		setIsProcessing({ preview: false, newGame: false, nextBoard: true });
-
 		try {
 			await goToNextBoard();
 		} catch (_) {
-			setIsProcessing({ preview: false, newGame: false,  nextBoard: false });
-
 			return;
 		}
-
-		setIsProcessing({ preview: false, newGame: false, nextBoard: false });
 	}, []);
-
-	// new game
 
 	const [showNewGameDialog, setShowNewGameDialog] = useState(false);
 
@@ -100,18 +60,14 @@ export const AdminControlsCard: React.FC<IProps> = ({ gameState, board, goToNext
 						title="Preview"
 						intent={Intent.PRIMARY}
 						onClick={onPreviewClick}
-						loading={processing.preview}
 					>
 						Preview
 					</Button>
-
-					{/*	Uncomment after feature merge */}
 
 					<Button
 						title="Preview"
 						intent={Intent.PRIMARY}
 						onClick={confirmNextBoard}
-						loading={processing.nextBoard}
 					>
 						Next Board
 					</Button>
@@ -120,7 +76,6 @@ export const AdminControlsCard: React.FC<IProps> = ({ gameState, board, goToNext
 						title="New Game"
 						intent={Intent.PRIMARY}
 						onClick={onNewGameClick}
-						loading={processing.newGame}
 					>
 						New Game
 					</Button>
@@ -136,7 +91,6 @@ export const AdminControlsCard: React.FC<IProps> = ({ gameState, board, goToNext
 				/>
 			)}
 
-			{/*	Uncomment after feature merge */}
 
 			{showConfirmNextBoardDialog && (
 				<ConfirmNextBoardDialog
