d5b0daba286c7fd32adbdc5f3cab58f5b8a38f84
Eduardo Iotti <ddz.iotti@gmail.com>
2023-01-16 22:47:26 -0300

    Create Update Preview Dialog

---
+++ src/Components/Pages/Game/Sidebar/AdminControlsCard/UpdatePreviewDialog/index.tsx
@@ -0,0 +1,178 @@
+import { Button, Dialog, HTMLTable, Intent } from '@blueprintjs/core';
+import { useCallback, useState, useEffect, useContext, useMemo } from 'react';
+import { Board } from '../../../../../../Api/Game-Catalog/Models/Boards';
+import { GamesModel, GameState, PlayerUpdate, UpdateResultType } from '../../../../../../Api/Game-State/Models/Games';
+import { UserContext } from '../../../../../../Session';
+import * as toaster from '../../../../../../Toaster';
+import { FrameLoadingSpinner } from '../../../../../FrameLoadingSpinner';
+
+interface IProps {
+	onClose: () => void;
+	gameState: GameState;
+	board: Board;
+}
+
+export const UpdatePreviewDialog: React.FC<IProps> = ({ gameState, board, onClose }) => {
+	const User = useContext(UserContext);
+
+	const [updateData, setUpdateData] = useState<PlayerUpdate[] | null>(null);
+
+	const sortedData = useMemo(() => {
+		if (updateData === null)
+			return [];
+
+		const deletedItems = updateData?.filter(item => item.type === UpdateResultType.DELETED);
+		const otherItems = updateData.filter(item => item.type !== UpdateResultType.DELETED);
+
+		otherItems.sort((playerA: PlayerUpdate, playerB: PlayerUpdate) => {
+			let playerAPoints;
+			let playerBPoints;
+
+			switch (playerA.type) {
+				case UpdateResultType.CHANGED:
+					playerAPoints = playerA.new_point_total;
+					break;
+				case UpdateResultType.MOVED:
+					playerAPoints = playerA.new_point_total;
+					break;
+				default:
+					playerAPoints = 0;
+					break;
+			}
+
+			switch (playerB.type) {
+				case UpdateResultType.CHANGED:
+					playerBPoints = playerB.new_point_total;
+					break;
+				case UpdateResultType.MOVED:
+					playerBPoints = playerB.new_point_total;
+					break;
+				default:
+					playerBPoints = 0;
+					break;
+			}
+
+			return playerAPoints - playerBPoints;
+		});
+
+		return [...otherItems, ...deletedItems];
+	}, [updateData]);
+
+	const fetchUpdateData = useCallback(async () => {
+		let updateData: PlayerUpdate[];
+
+		try {
+			updateData = await GamesModel.updatePreview(User!.account.id).then(response => response.data);
+		} catch (_) {
+			toaster.showUnhandledErrorMessage();
+
+			onClose();
+
+			return;
+		}
+
+		setUpdateData(updateData);
+	}, []);
+
+	useEffect(() => {
+		fetchUpdateData();
+	}, []);
+
+	const displayPlayerUpdateData = useCallback((player: PlayerUpdate) => {
+		const status = player.type;
+
+		let currentPlayer;
+		let playerId;
+		let playerName;
+		let currentPoints: string | number = '—';
+		let currentStage = '—';
+		let newPoints: string | number = '—';
+		let newStage = '—';
+
+		switch (player.type) {
+			case UpdateResultType.MOVED:
+				currentPlayer = player;
+				playerId = currentPlayer.player.hub_id;
+				playerName = currentPlayer.player.user_name;
+				currentPoints = currentPlayer.player.current_points;
+				currentStage = board.stages[currentPlayer.player.current_stage_index].name;
+				newStage = board.stages[currentPlayer.new_stage_index].name;
+				newPoints = currentPlayer.new_point_total ? currentPlayer.new_point_total : '—';
+				break;
+
+			case UpdateResultType.CHANGED:
+				currentPlayer = player;
+				playerId = currentPlayer.player.hub_id;
+				playerName = currentPlayer.player.user_name;
+				currentPoints = currentPlayer.player.current_points;
+				currentStage = board.stages[currentPlayer.player.current_stage_index].name;
+				newPoints = currentPlayer.new_point_total ? currentPlayer.new_point_total : '—';
+				break;
+
+			case UpdateResultType.CREATED:
+				currentPlayer = player;
+				playerId = currentPlayer.player.hub_id;
+				playerName = currentPlayer.player.user_name;
+				currentPoints = currentPlayer.player.current_points;
+				currentStage = board.stages[currentPlayer?.player.current_stage_index].name;
+				break;
+
+			case UpdateResultType.DELETED:
+				currentPlayer = gameState.players.find(p => p.hub_id === player.player_id);
+				playerId = player.player_id;
+				playerName = currentPlayer?.user_name;
+				currentPoints = currentPlayer!.current_points;
+				currentStage = currentPlayer!.current_stage_name;
+				break;
+
+			default:
+				break;
+		}
+
+		return (
+			<tr key={playerId} style={{}}>
+				<td>{playerName}</td>
+				<td style={{ textTransform: 'capitalize' }}>{status}</td>
+				<td>{currentPoints}</td>
+				<td>{currentStage}</td>
+				<td>{newPoints}</td>
+				<td>{newStage}</td>
+			</tr>
+		);
+	}, []);
+
+	if (!updateData) {
+		return (
+			<Dialog title="Update Preview" icon="control" isOpen={true} onClose={onClose}>
+				<div style={{ marginTop: '1rem' }}>
+					<FrameLoadingSpinner />
+				</div>
+			</Dialog>
+		);
+	} else {
+		return (
+			<Dialog title="Update Preview" icon="control" isOpen={true} onClose={onClose}>
+				<div style={{ display: 'flex', flexDirection: 'column', padding: '1rem' }}>
+					<HTMLTable striped>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Status</th>
+								<th>Current Points</th>
+								<th>Current Stage</th>
+								<th>New Points</th>
+								<th>New Stage</th>
+							</tr>
+						</thead>
+
+						<tbody>{sortedData.map(displayPlayerUpdateData)}</tbody>
+					</HTMLTable>
+				</div>
+
+				<Button onClick={onClose} intent={Intent.PRIMARY} style={{ margin: '0 1rem' }}>
+					Close
+				</Button>
+			</Dialog>
+		);
+	}
+};
