ece7637caff41ae1a77e2a7920c1887dcba8ef62
Larry <lbecker@gravitationalmarketing.com>
2023-02-07 10:05:27 -0500

    Update GameAnnouncment component to use PlayerUpdate

--- src/Components/Pages/Game/Board/GameAnnouncement.tsx
+++ src/Components/Pages/Game/Board/GameAnnouncement.tsx
@@ -1,26 +1,33 @@
 import {H1, H3} from '@blueprintjs/core';
 import * as React from 'react';
-import {PlayerState} from '../../../../Api/Game-State/Models/Games';
+import {Stage} from '../../../../Api/Game-Catalog/Models/Stages';
+import {PlayerUpdate, UpdateResultType} from '../../../../Api/Game-State/Models/Games';
+import {FrameLoadingSpinner} from '../../../FrameLoadingSpinner';
 import {classNames} from '../../../Utility/dom';
 import {ucwords} from '../../../Utility/string';
 import './GameAnnouncement.scss';
 
 interface IProps {
-	player: PlayerState | null;
+	player: PlayerUpdate | null;
+	stage: Stage | null;
 }
 
-export const GameAnnouncement: React.FC<IProps> = ({ player }) => {
+export const GameAnnouncement: React.FC<IProps> = ({ player, stage }) => {
+	// TODO: this is not very clean... Looks into another solution /Larry
+	if (player?.type === UpdateResultType.DELETED)
+		return <FrameLoadingSpinner />;
+
 	return (
 		<div
 			className={classNames('game-announcement-container', player && 'player-movement-toast')}
-			key={player?.hub_id}
+			key={player?.player.hub_id}
 		>
 			<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto'}}>
-				<H1>{ucwords(player?.user_name ?? 'Player Name')}</H1>
+				<H1>{ucwords(player?.player.user_name ?? 'Player Name')}</H1>
 
 				<H3>has moved to</H3>
 
-				<H1>{ucwords(player?.current_stage_name ?? 'Stage Name')}</H1>
+				<H1>{ucwords(stage?.name ?? 'Stage Name')}</H1>
 			</div>
 		</div>
 	);
