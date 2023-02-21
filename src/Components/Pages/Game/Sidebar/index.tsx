0ada661065aac2bc929cd6b1083e5072e4894870
Larry <lbecker@gravitationalmarketing.com>
2023-02-21 09:41:28 -0500

    Update Sidebar with start and next button

--- src/Components/Pages/Game/Sidebar/index.tsx
+++ src/Components/Pages/Game/Sidebar/index.tsx
@@ -1,16 +1,27 @@
+import * as React from 'react';
 import {Button, Icon, IconSize, Intent} from '@blueprintjs/core';
 import './Sidebar.scss';
 
 interface IProps {
 	children: React.ReactNode;
+	processing: boolean;
+	isNextButton: boolean;
+	onStartClick: () => void;
+	onNextClick: () => void;
 }
 
 export const Sidebar: React.FC<IProps> = props => (
 	<aside className="gm-sidebar">
 		<div>{props.children}</div>
 
-		<Button intent={Intent.PRIMARY} large>
-			Start <Icon icon="caret-right" size={IconSize.STANDARD} />
+		<Button
+			intent={Intent.PRIMARY}
+			onClick={props.isNextButton ? props.onStartClick : props.onNextClick}
+			large
+		>
+			{props.isNextButton ? 'Start' : 'Next'} <Icon icon="caret-right" size={IconSize.STANDARD} />
 		</Button>
 	</aside>
 );
+
+Sidebar.displayName = 'Sidebar';
