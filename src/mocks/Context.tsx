023e7831b5dbb69a125d06b1b1f99713ce90f7f6
Eduardo Iotti <ddz.iotti@gmail.com>
2023-03-03 15:41:41 -0300

    Add story for Point Summary Page

--- src/mocks/Context.tsx
+++ src/mocks/Context.tsx
@@ -3,9 +3,19 @@ import { Story } from '@storybook/react';
 import { UserContext } from '../Session';
 
 export function ContextMockDecorator(Story: Story) {
+	// hard-coded token with unlimited expiration
+	const token = '.eyJpYXQiOjE2Nzc3ODAzNjQsImV4cCI6MzIzMjM0MzQyMzIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VySWRlbnRpZmllciI6InRlc3RAZXhhbXBsZS5jb20iLCJpZCI6MSwiYWNjb3VudElkIjoxLCJwZXJtaXNzaW9ucyI6WyJhZG1pbiJdfQ==.'
+
+	window.localStorage.setItem(
+		'api.auth_token',
+		token
+	);
+
 	return (
-		<UserContext.Provider value={userMock}>
-			<Story />
-		</UserContext.Provider>
+		<div className="bp4-dark">
+			<UserContext.Provider value={userMock}>
+				<Story />
+			</UserContext.Provider>
+		</div>
 	);
 }
---
+++ src/mocks/PointSourceItem.ts
@@ -0,0 +1,11 @@
+import { PointSourceItem } from '../Api/Point-Tracking/Models/Sources';
+
+export const pointSourceItemsMock: PointSourceItem[] = [
+	{
+		id: {
+			$oid: '0',
+		},
+		name: 'Test Source',
+		point_value: 123,
+	},
+];
