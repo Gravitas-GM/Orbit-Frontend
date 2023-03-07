509088443eab09f3661cab09a52e8f0bee307d87
Eduardo Iotti <ddz.iotti@gmail.com>
2023-03-07 12:59:00 -0300

    Adjust JWT token

--- src/mocks/Context.tsx
+++ src/mocks/Context.tsx
@@ -4,7 +4,7 @@ import { UserContext } from '../Session';
 
 export function ContextMockDecorator(Story: Story) {
 	// hard-coded token with unlimited expiration
-	const token = '.eyJpYXQiOjE2Nzc3ODAzNjQsImV4cCI6MzIzMjM0MzQyMzIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VySWRlbnRpZmllciI6InRlc3RAZXhhbXBsZS5jb20iLCJpZCI6MSwiYWNjb3VudElkIjoxLCJwZXJtaXNzaW9ucyI6WyJhZG1pbiJdfQ==.'
+	const token = '.eyJpYXQiOjE2Nzc3ODAzNjQsImV4cCI6MzIzMjM0MzQyMzIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VySWRlbnRpZmllciI6InRlc3RAZXhhbXBsZS5jb20iLCJpZCI6MSwiYWNjb3VudElkIjoxLCJwZXJtaXNzaW9ucyI6WyJhZG1pbiJdfQ.'
 
 	window.localStorage.setItem(
 		'api.auth_token',
