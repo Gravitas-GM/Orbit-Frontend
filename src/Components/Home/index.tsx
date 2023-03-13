e1a08904eabda19a1b064a7a57a829d819805c15
Eduardo Iotti <ddz.iotti@gmail.com>
2023-03-13 16:44:10 -0300

    Styling for Home cards

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Classes, Divider, H2, H5 } from '@blueprintjs/core';
+import { Card, H2, H4, Icon } from '@blueprintjs/core';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Permission, PermissionContext  } from '../../Permission';
@@ -10,33 +10,42 @@ export const Home: React.FC = () => (
 		{() => (
 			<PermissionContext.Consumer>
 				{([isGranted]) => (
-					<div className="gm-page-wrapper">
-						Welcome to Orbit!
+					<div className="gm-page-wrapper home-page-container">
+						<H2>Welcome to Orbit!</H2>
 
-						<H2 style={{marginTop: '2rem' }}>Game</H2>
+						<H4 style={{marginTop: '2rem' }}>Game</H4>
 
 						<div className="cards-container">
 							<Link to="/catalog">
-								<Card>
-									<H5>Game Catalog</H5>
-
-									<p>Browse our Game Catalog and start playing one of our games</p>
+								<Card interactive={true}>
+									<Icon icon="layers"/>
+									<div>
+										<H4>Game Catalog</H4>
+										<p>Browse our Game Catalog and start playing one of our games</p>
+									</div>
 								</Card>
 							</Link>
 
 							<Link to="/game">
-								<Card>
-									<H5>Game Board</H5>
+								<Card interactive={true}>
+									<Icon icon='star'/>
+									<div>
+										<H4>Game Board</H4>
 
-									<p>Continue playing on your current game board.</p>
+										<p>Continue playing on your current game board.</p>
+									</div>
 								</Card>
 							</Link>
 
 							<Link to="/leaderboard">
-								<Card>
-									<H5>Leaderboard</H5>
+								<Card interactive={true}>
+									<Icon icon="properties"/>
 
-									<p>See how users rank against each other.</p>
+									<div>
+										<H4>Leaderboard</H4>
+
+										<p>See how users rank against each other.</p>
+									</div>
 								</Card>
 							</Link>
 						</div>
@@ -44,25 +53,30 @@ export const Home: React.FC = () => (
 						{
 							isGranted(Permission.ADMIN) &&
 							<>
-								<H2 style={{marginTop: '2rem' }}>Admin</H2>
+								<H4 style={{marginTop: '2rem' }}>Admin</H4>
 
 
 								<div className="cards-container admin">
-
 									<Link to="/users">
-										<Card>
-											<H5>Users List</H5>
+										<Card interactive={true}>
+											<Icon icon="people"/>
+											<div>
+												<H4>Users List</H4>
 
-											<p>Browse users list, manage users, and give points.</p>
+												<p>Browse users list, manage users, and give points.</p>
+											</div>
 										</Card>
 									</Link>
 
 
 									<Link to="/sources">
-										<Card>
-											<H5>Sources</H5>
+										<Card interactive={true}>
+											<Icon icon="bank-account"/>
+											<div>
+												<H4>Sources</H4>
 
-											<p>Manage point sources and give points to multiple users.</p>
+												<p>Manage point sources and give points to multiple users.</p>
+											</div>
 										</Card>
 									</Link>
 								</div>
