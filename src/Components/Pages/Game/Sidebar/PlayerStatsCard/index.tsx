3d0a4cd42cbb46c30d04b8c1af9470dbe45099a3
Eduardo Iotti <ddz.iotti@gmail.com>
2023-03-27 18:30:18 -0300

    Fix sidebar and cards spacings and shadow artifact

--- src/Components/Pages/Game/Sidebar/PlayerStatsCard/index.tsx
+++ src/Components/Pages/Game/Sidebar/PlayerStatsCard/index.tsx
@@ -18,7 +18,7 @@ export const PlayerStatsCard: React.FC<IProps> = ({player}) => {
 
 	return (
 		<GameCard title="Player Stats" icon="user">
-			<div style={{display: 'flex', flexDirection: 'column'}}>
+			<div className="card-content-wrapper">
 				<span style={{marginBottom: '1rem'}}>
 					{player.user_name} ({player.current_points} points)
 				</span>
