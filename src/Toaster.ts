9c9b5c7fe93d41158344176f070fb88f8e093d8b
Eduardo Iotti <ddz.iotti@gmail.com>
2023-04-04 13:17:32 -0300

    Change toaster position for all toasts

--- src/Toaster.ts
+++ src/Toaster.ts
@@ -1,88 +1,61 @@
-import {Intent, IToaster, IToastProps as BaseProps, Position, Toaster, ToasterPosition} from '@blueprintjs/core';
+import {Intent, IToastProps, Position, Toaster} from '@blueprintjs/core';
 import { NextBoardResult } from './Api/Game-State/Models/Games';
 
-const defaultToaster: IToaster = Toaster.create({
-	position: Position.BOTTOM_RIGHT,
+const toaster = Toaster.create({
+	position: Position.BOTTOM_LEFT,
 });
 
-
-interface IToastProps extends BaseProps {
-	position?: ToasterPosition;
-}
-
 export function show(props: IToastProps) {
-	if (!!props.position) {
-		const customToaster = Toaster.create({
-			position: props.position
-		})
-
-		customToaster.show(props);
-
-		return;
-	}
-
-	defaultToaster.show(props);
+	toaster.show(props);
 }
 
-export function success(message: string, position?: ToasterPosition) {
-	show(
-		{
+export function success(message: string) {
+	show({
 		intent: Intent.SUCCESS,
 		message,
-		position
-		}, 
-	);
+	});
 }
 
-export function info(message: string, position?: ToasterPosition) {
-	show(
-		{
-			intent: Intent.PRIMARY,
-			message,
-			position
-		},
-	);
+export function info(message: string) {
+	show({
+		intent: Intent.PRIMARY,
+		message,
+	});
 }
 
-export function error(message: string, position?: ToasterPosition) {
-	show(
-		{
-			intent: Intent.DANGER,
-			message,
-			position
-		},
-	);
+export function error(message: string) {
+	show({
+		intent: Intent.DANGER,
+		message,
+	});
 }
 
-export function warning(message: string, position?: ToasterPosition) {
-	show(
-		{
-			intent: Intent.WARNING,
-			message,
-			position
-		},
-	);
+export function warning(message: string) {
+	show({
+		intent: Intent.WARNING,
+		message,
+	});
 }
 
 // region Error Messages
-export function showUnhandledErrorMessage(position?: ToasterPosition) {
-	error('An error occurred while processing your request. Please try again.', position);
+export function showUnhandledErrorMessage() {
+	error('An error occurred while processing your request. Please try again.');
 }
 
-export function showValidationFailedErrorMessage(position?: ToasterPosition) {
-	error('One or more fields did not pass validation.', position);
+export function showValidationFailedErrorMessage() {
+	error('One or more fields did not pass validation.');
 }
 
 
 const nextBoardResultMessage = {
-	[NextBoardResult.Success]: (position?: ToasterPosition) => success('Moved to next board.', position),
-	[NextBoardResult.NoActiveGame]: (position?: ToasterPosition) => info('There is no active game.', position),
-	[NextBoardResult.NoRemainingBoards]: (position?: ToasterPosition) => info('There are no boards remaining.', position),
-	[NextBoardResult.BoardNotFound]: (position?: ToasterPosition) => error('Couldn\'t find the specified board.', position),
+	[NextBoardResult.Success]: () => success('Moved to next board.'),
+	[NextBoardResult.NoActiveGame]: () => info('There is no active game.'),
+	[NextBoardResult.NoRemainingBoards]: () => info('There are no boards remaining.'),
+	[NextBoardResult.BoardNotFound]: () => error('Couldn\'t find the specified board.'),
 };
 
-export function notifyNextBoardResult(result: NextBoardResult, position?: ToasterPosition) {
-	nextBoardResultMessage[result](position);
+export function notifyNextBoardResult(result: NextBoardResult) {
+	nextBoardResultMessage[result]();
 }
 
 // endregion
