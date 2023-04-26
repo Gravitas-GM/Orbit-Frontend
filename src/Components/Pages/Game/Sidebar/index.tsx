2f3ab2b7d0312a8b329fb171e72295700b1b7051
Eduardo Iotti <ddz.iotti@gmail.com>
2023-04-26 16:30:42 -0300

    Add simplebar to entire Sidebar component

--- src/Components/Pages/Game/Sidebar/index.tsx
+++ src/Components/Pages/Game/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Button, Icon, IconSize, Intent} from '@blueprintjs/core';
 import './Sidebar.scss';
+import SimpleBar from 'simplebar-react';
 
 interface IProps {
 	children: React.ReactNode;
@@ -10,17 +11,19 @@ interface IProps {
 }
 
 export const Sidebar: React.FC<IProps> = props => (
-	<aside className="gm-sidebar">
-		<div>{props.children}</div>
+	<SimpleBar style={{ maxHeight: 'calc(100vh - 50px)'}}>
+		<aside className="gm-sidebar">
+			<div>{props.children}</div>
 
-		<Button
-			intent={Intent.PRIMARY}
-			onClick={props.onButtonClick}
-			large
-		>
-			{props.buttonLabel} <Icon icon="caret-right" size={IconSize.STANDARD} />
-		</Button>
-	</aside>
+			<Button
+				intent={Intent.PRIMARY}
+				onClick={props.onButtonClick}
+				large
+			>
+				{props.buttonLabel} <Icon icon="caret-right" size={IconSize.STANDARD} />
+			</Button>
+		</aside>
+	</SimpleBar>
 );
 
 Sidebar.displayName = 'Sidebar';
