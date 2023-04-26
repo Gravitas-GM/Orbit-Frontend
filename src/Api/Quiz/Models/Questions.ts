c76985ded69246b944db9fb8c17a97d34233def4
Larry <lbecker@gravitationalmarketing.com>
2023-04-26 09:45:07 -0400

    Add quiz Questions model

---
+++ src/Api/Quiz/Models/Questions.ts
@@ -0,0 +1,44 @@
+import {User} from './Users';
+
+export interface QuestionEndpoints {
+	// TODO: Add endpoints
+}
+
+interface BaseQuestion {
+	id: number,
+	tagId: number|null,
+	prompt: string,
+	kind: QuestionKind,
+}
+
+export enum QuestionKind {
+	FreeText = 'free text',
+	Boolean = 'boolean',
+	MultipleChoice = 'multiple choice',
+}
+
+export interface FreeTextQuestion extends BaseQuestion {
+	kind: QuestionKind.FreeText,
+	answers: string[],
+}
+
+export interface BooleanQuestion extends BaseQuestion {
+	kind: QuestionKind.Boolean,
+	answer: boolean,
+	trueLabel: string|null,
+	falseLabel: string|null,
+}
+
+export interface MultipleChoiceQuestion extends BaseQuestion {
+	kind: QuestionKind.MultipleChoice,
+	choices: string[],
+	answerIndex: number,
+}
+
+export type Question = FreeTextQuestion | BooleanQuestion | MultipleChoiceQuestion;
+
+export interface QuestionTag {
+	id: number,
+	label: string,
+	members: User[],
+}
