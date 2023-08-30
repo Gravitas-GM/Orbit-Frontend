7cee4642a32ac86b75bc62e42fb385f7ecacc197
Larry <lbecker@gravitationalmarketing.com>
2023-08-30 09:37:35 -0400

    Update Accounts model

--- src/Api/Quiz/Models/Accounts.ts
+++ src/Api/Quiz/Models/Accounts.ts
@@ -1,21 +1,6 @@
-import {Id, Projectable, Projection, quizClient} from '../../index';
+import {Id, quizClient} from '../../index';
 
 export interface AccountEndpoints {
-	'/accounts': {
-		PUT: {
-			query: Projectable;
-			body: Account;
-			response: Account;
-		};
-	};
-
-	'/accounts/:account': {
-		DELETE: {
-			params: Id;
-			response: void;
-		};
-	};
-
 	'/settings/:account': {
 		GET: {
 			params: Id;
@@ -27,11 +12,11 @@ export interface AccountEndpoints {
 			body: AccountUpdatePayload;
 			response: Account;
 		};
-	}
+	};
 }
 
 export interface Account {
-	accountId: number,
+	id: number,
 	quizFrequency: Frequency,
 	questionCount: number,
 	completedRewardPointSourceId: string | null,
@@ -43,26 +28,14 @@ export enum Frequency {
 	Monthly = 'monthly',
 }
 
-export type AccountUpdatePayload = Partial<Omit<Account, 'accountId'>>;
+export type AccountUpdatePayload = Partial<Omit<Account, 'id'>>;
 
 export class AccountModel {
-	public static create(payload: Account, projection?: Projection) {
-		return quizClient.put('/accounts', payload, {
-			params: {
-				p: projection,
-			},
-		});
-	}
-
 	public static read(account: Id) {
 		return quizClient.get<'/settings/:account'>(`/settings/${account}`);
 	}
 
 	public static update(account: Id, payload: AccountUpdatePayload) {
 		return quizClient.patch<'/settings/:account'>(`/settings/${account}`, payload);
-	}
-
-	public static delete(account: Id) {
-		return quizClient.delete<'/accounts/:account'>(`/accounts/${account}`);
 	}
 }
