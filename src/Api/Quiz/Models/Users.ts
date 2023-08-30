d4c52ceffdb88b98412d4128ad3280bce1d855df
Larry <lbecker@gravitationalmarketing.com>
2023-08-30 09:38:04 -0400

    Update Users model

--- src/Api/Quiz/Models/Users.ts
+++ src/Api/Quiz/Models/Users.ts
@@ -1,39 +1,26 @@
-import {Id, Projectable, Projection, quizClient} from '../../index';
+import {Id, quizClient} from '../../index';
 import {QuestionTag} from './QuestionTags';
 import {QuizSubmission} from './QuizSubmissions';
 
 export interface UserEndpoints {
-	'/users': {
-		PUT: {
-			query: Projectable;
-			body: UserCreatePayload;
-			response: User;
-		};
-	};
-
 	'/users/:id': {
 		GET: {
 			params: Id;
 			response: User;
 		};
+	};
 
-		PATCH: {
-			params: Id;
-			body: UserUpdatePayload;
+	'/users/me': {
+		GET: {
 			response: User;
-		};
-
-		DELETE: {
-			params: Id;
-			response: void;
-		};
+		}
 	};
 
 	'/users/me/submissions': {
 		GET: {
 			response: QuizSubmission[];
 		}
-	}
+	};
 }
 
 export interface User {
@@ -43,29 +30,13 @@ export interface User {
 	assignedTags: QuestionTag[],
 }
 
-export type UserCreatePayload = Omit<User, 'id'>;
-
-export type UserUpdatePayload = Partial<Omit<User, 'id'>>;
-
 export class UserModel {
-	public static create(payload: UserCreatePayload, projection?: Projection) {
-		return quizClient.put('/users', payload, {
-			params: {
-				p: projection,
-			},
-		});
-	}
-
 	public static read(user: Id) {
 		return quizClient.get<'/users/:id'>(`/users/${user}`);
 	}
 
-	public static update(user: Id, payload: UserUpdatePayload) {
-		return quizClient.patch<'/users/:id'>(`/users/${user}`, payload);
-	}
-
-	public static delete(user: Id) {
-		return quizClient.delete<'/users/:id'>(`/users/${user}`);
+	public static getCurrentUser() {
+		return quizClient.get('/users/me');
 	}
 
 	public static getSubmissions() {
