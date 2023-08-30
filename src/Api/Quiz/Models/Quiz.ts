68090d4aef38a87aee3cf81ef5b18aa38710af3f
Larry <lbecker@gravitationalmarketing.com>
2023-08-30 09:37:51 -0400

    Update Quiz model

--- src/Api/Quiz/Models/Quiz.ts
+++ src/Api/Quiz/Models/Quiz.ts
@@ -1,24 +1,75 @@
 import {quizClient} from '../../index';
-import {Question} from './Questions';
-import {QuizSubmission, QuizSubmissionCreatePayload} from './QuizSubmissions';
+import {QuestionKind} from './Questions';
+import {QuizSubmission} from './QuizSubmissions';
 
 export interface QuizEndpoints {
 	'/quiz/start': {
 		POST: {
 			response: Quiz;
 		};
-	}
+	};
 
 	'/quiz/finish': {
 		POST: {
-			body: QuizSubmissionCreatePayload;
+			body: QuizFinishPayload;
 			response: QuizSubmission;
 		};
-	}
+	};
 }
 
 export interface Quiz {
-	questions: Question[],
+	questions: QuestionPrompt[],
+	endTimestamp: Date,
+}
+
+interface QuestionPromptBase {
+	id: number,
+	prompt: string,
+	kind: QuestionKind,
+}
+
+export interface FreeTextQuestionPrompt extends QuestionPromptBase {
+	kind: QuestionKind.FreeText,
+}
+
+export interface BooleanQuestionPrompt extends QuestionPromptBase {
+	kind: QuestionKind.Boolean,
+	trueLabel: string | null,
+	falseLabel: string | null,
+}
+
+export interface MultipleChoiceQuestionPrompt extends QuestionPromptBase {
+	kind: QuestionKind.MultipleChoice,
+	choices: string[],
+}
+
+export type QuestionPrompt = FreeTextQuestionPrompt | BooleanQuestionPrompt | MultipleChoiceQuestionPrompt;
+
+
+interface AnswerBase {
+	id: number,
+	kind: QuestionKind,
+}
+
+export interface FreeTextAnswer extends AnswerBase {
+	kind: QuestionKind.FreeText,
+	response: string,
+}
+
+export interface BooleanAnswer extends AnswerBase {
+	kind: QuestionKind.Boolean,
+	response: boolean,
+}
+
+export interface MultipleChoiceAnswer extends AnswerBase {
+	kind: QuestionKind.MultipleChoice,
+	response: number,
+}
+
+export type Answer = FreeTextAnswer | BooleanAnswer | MultipleChoiceAnswer;
+
+export interface QuizFinishPayload {
+	answers: Answer[],
 }
 
 export class QuizModel {
