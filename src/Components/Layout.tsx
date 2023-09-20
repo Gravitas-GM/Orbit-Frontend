a3d6e2ff4c1274c7c965047553de4fbaebaa9739
Tyler Lartonoix <tyler@lartonoix.com>
2023-09-20 11:44:49 -0400

    Update nav menu to use Popover2

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -30,7 +30,7 @@ export const Layout: React.FC<IProps> = props => (
 		<FrameLoadingSpinner />
 	) : (
 		<div style={{flex: 12, height: '100%'}}>
-			<NavHeader loading={props.loading} />
+			<NavHeader />
 
 			<div className="main-frame">
 				<PermissionContext.Consumer>
