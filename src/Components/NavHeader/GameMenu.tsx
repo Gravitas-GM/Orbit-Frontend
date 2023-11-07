bca694e33fcaa7d72f2a8ab992e84408fd1c184b
Larry <lbecker@gravitationalmarketing.com>
2023-11-07 08:41:23 -0500

    Restructure navbar

--- src/Components/NavHeader/GameMenu.tsx
+++ src/Components/NavHeader/GameMenu.tsx
@@ -1,41 +1,25 @@
 import * as React from 'react';
-import {MenuItem2 as MenuItem} from '@blueprintjs/popover2/lib/esm/menuItem2';
 import {Permission, PermissionContext} from '../../Permission';
 import {Menu, MenuDivider} from '@blueprintjs/core';
-import {UserClaimPointsDialog} from '../Pages/UserClaimPointsDialog';
 import {LinkedMenuItem} from './LinkedMenuItem';
 
 export const GameMenu: React.FC = () => {
 	const [isGranted] = React.useContext(PermissionContext);
-	const [showDialog, setShowDialog] = React.useState(false);
-
-	const onDialogOpen = React.useCallback(() => setShowDialog(true), []);
-	const onDialogClose = React.useCallback(() => setShowDialog(false), []);
 
 	return (
-		<>
-			<Menu>
-				<MenuItem
-					icon="plus"
-					text="Claim Points"
-					onClick={onDialogOpen}
-				/>
+		<Menu>
+			<LinkedMenuItem to="/game" icon="star" text="Game Board" />
+			<LinkedMenuItem to="/leaderboard" icon="properties" text="Leaderboard" />
 
-				<LinkedMenuItem to="/game" icon="star" text="Game Board" />
-				<LinkedMenuItem to="/leaderboard" icon="properties" text="Leaderboard" />
+			{isGranted(Permission.ADMIN) && (
+				<>
+					<MenuDivider />
 
-				{isGranted(Permission.ADMIN) && (
-					<>
-						<MenuDivider />
-
-						<LinkedMenuItem to="/catalog" icon="layers" text="Game Catalog" />
-						<LinkedMenuItem to="/sources" icon="bank-account" text="Sources" />
-					</>
-				)}
-			</Menu>
-
-			<UserClaimPointsDialog onClose={onDialogClose} isOpen={showDialog} />
-		</>
+					<LinkedMenuItem to="/catalog" icon="layers" text="Game Catalog" />
+					<LinkedMenuItem to="/sources" icon="bank-account" text="Sources" />
+				</>
+			)}
+		</Menu>
 	);
 };
 
--- src/Components/NavHeader/index.tsx
+++ src/Components/NavHeader/index.tsx
@@ -1,18 +1,23 @@
+import * as React from 'react';
 import {Alignment, Button, Classes, Icon, IconSize, Intent, Navbar, Spinner} from '@blueprintjs/core';
 import {Popover2 as Popover} from '@blueprintjs/popover2';
-import * as React from 'react';
 import {Link} from 'react-router-dom';
 import {Permission, PermissionContext} from '../../Permission';
 import {UserContext} from '../../Session';
-import './index.scss';
+import {UserClaimPointsDialog} from '../Pages/UserClaimPointsDialog';
 import {renderUserName} from '../Utility/string';
 import {GameMenu} from './GameMenu';
 import {QuizMenu} from './QuizMenu';
 import {UserMenu} from './UserMenu';
+import './index.scss';
 
 export const NavHeader: React.FC = () => {
 	const user = React.useContext(UserContext);
 	const [isGranted] = React.useContext(PermissionContext);
+	const [showDialog, setShowDialog] = React.useState(false);
+
+	const onDialogOpen = React.useCallback(() => setShowDialog(true), []);
+	const onDialogClose = React.useCallback(() => setShowDialog(false), []);
 
 	return (
 		<>
@@ -43,13 +48,24 @@ export const NavHeader: React.FC = () => {
 						<Button text="Quiz" minimal={true} rightIcon="caret-down" />
 					</Popover>
 
+					<Button
+						icon="plus"
+						text="Claim Points"
+						minimal={true}
+						onClick={onDialogOpen}
+					/>
+
 					{isGranted(Permission.ADMIN) && (
-						<Link to="/users">
-							<Button
-								text="Users"
-								minimal={true}
-							/>
-						</Link>
+						<>
+							<Navbar.Divider />
+
+							<Link to="/users">
+								<Button
+									text="Users"
+									minimal={true}
+								/>
+							</Link>
+						</>
 					)}
 
 				</Navbar.Group>
@@ -68,6 +84,8 @@ export const NavHeader: React.FC = () => {
 					) : <Spinner size={20} intent={Intent.PRIMARY} />}
 				</Navbar.Group>
 			</Navbar>
+
+			<UserClaimPointsDialog onClose={onDialogClose} isOpen={showDialog} />
 		</>
 	);
 };
