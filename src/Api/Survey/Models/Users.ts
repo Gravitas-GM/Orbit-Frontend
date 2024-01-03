ec8c3c59ffbb8b47f125bd08497830899917dfcf
Larry <lbecker@gravitationalmarketing.com>
2024-01-03 14:04:34 -0500

    Remove update from Survey User

--- src/Api/Survey/Models/Users.ts
+++ src/Api/Survey/Models/Users.ts
@@ -1,17 +1,10 @@
 import {Department} from '../../Hub/Models/Departments';
-import {Id, Projectable, Projection, surveyClient} from '../../index';
+import {Id, Projection, surveyClient} from '../../index';
 
 export interface UserEndpoints {
 	'/users/:user': {
 		GET: {
 			params: Id;
-			response: User;
-		};
-
-		PATCH: {
-			query: Projectable;
-			params: Id;
-			body: UserUpdatePayload;
 			response: User;
 		};
 	};
@@ -23,21 +16,9 @@ export interface User {
 	surveySubmitted: boolean;
 }
 
-export type UserUpdatePayload = Partial<Omit<User, 'id' | 'department'> & {
-	department?: number | null;
-}>;
-
 export class UserModel {
 	public static read(user: Id, projection?: Projection) {
 		return surveyClient.get<'/users/:user'>(`/users/${user}`, {
-			params: {
-				p: projection,
-			},
-		});
-	}
-
-	public static update(user: Id, payload: UserUpdatePayload, projection?: Projection) {
-		return surveyClient.patch<'/users/:user'>(`/users/${user}`, payload, {
 			params: {
 				p: projection,
 			},
