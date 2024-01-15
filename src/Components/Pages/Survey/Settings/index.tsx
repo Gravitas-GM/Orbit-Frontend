02f7543dca5176772b7e7d58f1d333a201a25192
Larry <lbecker@gravitationalmarketing.com>
2024-01-15 09:12:40 -0500

    Create Survey Settings page

---
+++ src/Components/Pages/Survey/Settings/index.tsx
@@ -0,0 +1,152 @@
+import * as React from 'react';
+import {Button, Intent, Switch} from '@blueprintjs/core';
+import {MenuItem2 as MenuItem} from '@blueprintjs/popover2';
+import {ItemRenderer} from '@blueprintjs/select';
+import {Prompt} from 'react-router';
+import {isValidationFailureError, ValidationFailures} from '../../../../Api/errors/symfony';
+import {DayOfWeek, Settings, SettingsModel} from '../../../../Api/Survey/Models/Settings';
+import {Classes} from '../../../../classes';
+import {history} from '../../../../history';
+import {UserContext} from '../../../../Session';
+import {toaster} from '../../../../toaster';
+import {FrameLoadingSpinner} from '../../../FrameLoadingSpinner';
+import {PageHeader} from '../../../PageHeader';
+import {Select} from '../../../Select/Select';
+import {ucwords} from '../../../Utility/string';
+import {ValidationAwareFormGroup} from '../../../ValidationAwareFormGroup';
+
+interface IState {
+	loading: boolean;
+	processing: boolean;
+	failures: ValidationFailures | null;
+	surveyRefreshDay: DayOfWeek;
+	dirty: boolean;
+}
+
+export class SurveySettings extends React.PureComponent<{}, IState> {
+	public static contextType = UserContext;
+	declare context: React.ContextType<typeof UserContext>;
+
+	public readonly state: IState = {
+		loading: true,
+		processing: false,
+		failures: null,
+		surveyRefreshDay: DayOfWeek.MONDAY,
+		dirty: false,
+	};
+
+	public async componentDidMount() {
+		let settings: Settings;
+
+		try {
+			settings = await SettingsModel.read(this.context!.account.id).then(r => r.data);
+		} catch (error) {
+			toaster.error('Failed to load survey settings.');
+			history.push('/');
+
+			return;
+		}
+
+		this.setState({
+			loading: false,
+			surveyRefreshDay: settings.surveyRefreshDay,
+		});
+	}
+
+	public render() {
+		if (this.state.loading)
+			return <FrameLoadingSpinner />;
+
+		return (
+			<section className={Classes.PAGE_WRAPPER}>
+				<PageHeader title="Survey Settings" />
+
+				<form onSubmit={this.onSaveButtonClick} style={{maxWidth: 800}}>
+					<ValidationAwareFormGroup
+						label="Survey Refresh Day"
+						labelFor="surveyRefreshDay"
+						failures={this.state.failures}
+					>
+						<div className={Classes.FORM_GROUP_SUB_LABEL}>
+							Sets what day of the week the survey will reset on.
+						</div>
+
+						<Select
+							items={Object.values(DayOfWeek)}
+							onItemSelect={this.onRefreshDayChange}
+							filterable={false}
+							itemRenderer={this.renderDayOfWeekItem}
+							fill={true}
+						>
+							<Button
+								fill={true}
+								text={ucwords(this.state.surveyRefreshDay)}
+								rightIcon="caret-down"
+								placeholder="Select survey refresh day"
+								alignText="left"
+							/>
+						</Select>
+					</ValidationAwareFormGroup>
+
+					<Button loading={this.state.processing} type="submit" intent={Intent.PRIMARY} text="Save" />
+				</form>
+
+				<Prompt when={this.state.dirty} message="You have unsaved changes. Are you sure you want to leave?" />
+			</section>
+		);
+	}
+
+	private onRefreshDayChange = (surveyRefreshDay: DayOfWeek) => this.setState({
+		surveyRefreshDay,
+		dirty: true,
+	});
+
+	private onSaveButtonClick = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		if (this.state.processing)
+			return;
+
+		this.setState({
+			processing: true,
+			failures: null,
+		});
+
+		try {
+			await SettingsModel.update(this.context!.account.id, {
+				surveyRefreshDay: this.state.surveyRefreshDay,
+			});
+		} catch (error) {
+			if (isValidationFailureError(error)) {
+				toaster.showValidationFailedErrorMessage();
+
+				this.setState({
+					failures: error.context.failures,
+				});
+			} else
+				toaster.showUnhandledErrorMessage();
+
+			return;
+		} finally {
+			this.setState({
+				processing: false,
+				dirty: false,
+			});
+		}
+
+		toaster.success('Survey settings saved.');
+	};
+
+	private renderDayOfWeekItem: ItemRenderer<DayOfWeek> = (item, {handleClick, handleFocus, modifiers}) => (
+		<MenuItem
+			key={item}
+			text={ucwords(item)}
+			selected={item === this.state.surveyRefreshDay}
+			active={modifiers.active}
+			disabled={modifiers.disabled}
+			onClick={handleClick}
+			onFocus={handleFocus}
+			roleStructure="listoption"
+		/>
+	);
+}
