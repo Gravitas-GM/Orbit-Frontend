5dbd58a3bb233fc0bed85323104ff9c89c1a4967
Eduardo Iotti <ddz.iotti@gmail.com>
2024-01-16 15:33:47 -0300

    Use context API instead of consumer component

--- src/Components/Pages/Quiz/History/UserSelect.tsx
+++ src/Components/Pages/Quiz/History/UserSelect.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import {Button} from '@blueprintjs/core';
-import {ItemRenderer} from '@blueprintjs/select';
-import {User} from '../../../../Api/Hub/Models/Users';
-import {PermissionContext, Permission} from '../../../../Permission';
-import {Select} from '../../../Select/Select';
-import {MenuItem2 as MenuItem} from '@blueprintjs/popover2';
-import {ucwords} from '../../../Utility/string';
+import * as React from "react";
+import {Button} from "@blueprintjs/core";
+import {ItemRenderer} from "@blueprintjs/select";
+import {User} from "../../../../Api/Hub/Models/Users";
+import {PermissionContext, Permission} from "../../../../Permission";
+import {Select} from "../../../Select/Select";
+import {MenuItem2 as MenuItem} from "@blueprintjs/popover2";
+import {ucwords} from "../../../Utility/string";
 
 interface IProps {
 	users: User[];
@@ -15,41 +15,37 @@ interface IProps {
 }
 
 export class UserSelect extends React.PureComponent<IProps, {}> {
-	public render(){
-		const {users, onUserClear, onUserSelect, selectedUser} = this.props;
+	public static contextType = PermissionContext;
+	declare context: React.ContextType<typeof PermissionContext>;
+
+	public render() {
+		const [isGranted] = this.context;
+		const { users, onUserClear, onUserSelect, selectedUser } = this.props;
+
+		if (!isGranted(Permission.ADMIN))
+			return;
 
 		return (
-			<PermissionContext.Consumer>
-				{([isGranted]) => (
-					isGranted(Permission.ADMIN) && (
-						<div className="history-filter">
-							<Select<User>
-								items={users}
-								itemRenderer={this.renderUserOption}
-								onItemSelect={onUserSelect}
-								onClear={onUserClear}
-								noResults={(
-									<MenuItem
-										disabled={true}
-										text="No results."
-										roleStructure="listoption"
-									/>
-								)}
-							>
-								<Button
-									text={selectedUser ? `Show only ${selectedUser.firstName} ${selectedUser.lastName}` : 'Show All Users'}
-									rightIcon="caret-down"
-									alignText="left"
-									fill={true}
-								/>
-							</Select>
-						</div>
-					))}
-			</PermissionContext.Consumer>
-		)
+			<div className="history-filter">
+				<Select<User>
+					items={users}
+					itemRenderer={this.renderUserOption}
+					onItemSelect={onUserSelect}
+					onClear={onUserClear}
+					noResults={<MenuItem disabled={true} text="No results." roleStructure="listoption" />}
+				>
+					<Button
+						text={selectedUser ? `Show only ${selectedUser.firstName} ${selectedUser.lastName}` : "Show All Users"}
+						rightIcon="caret-down"
+						alignText="left"
+						fill={true}
+					/>
+				</Select>
+			</div>
+		);
 	}
 
-	private renderUserOption: ItemRenderer<User> = (user, {handleClick, handleFocus, modifiers}) => {
+	private renderUserOption: ItemRenderer<User> = (user, { handleClick, handleFocus, modifiers }) => {
 		if (!modifiers.matchesPredicate)
 			return null;
 
