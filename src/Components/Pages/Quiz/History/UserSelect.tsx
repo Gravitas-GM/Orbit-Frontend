4ea03238144f68f4d318030ad78a3f4cc1c94d9c
Eduardo Iotti <ddz.iotti@gmail.com>
2024-01-16 15:18:45 -0300

    Convert UserSelect to PureComponent and fix dropdown styling

--- src/Components/Pages/Quiz/History/UserSelect.tsx
+++ src/Components/Pages/Quiz/History/UserSelect.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {Button} from '@blueprintjs/core';
 import {ItemRenderer} from '@blueprintjs/select';
 import {User} from '../../../../Api/Hub/Models/Users';
-import {QuizSubmission} from '../../../../Api/Quiz/Models/QuizSubmissions';
 import {PermissionContext, Permission} from '../../../../Permission';
 import {Select} from '../../../Select/Select';
 import {MenuItem2 as MenuItem} from '@blueprintjs/popover2';
@@ -12,52 +11,59 @@ interface IProps {
 	users: User[];
 	onUserSelect: (user: User) => void;
 	onUserClear: () => void;
-	filteredSubmissions: QuizSubmission[] | null;
+	selectedUser: User | null;
 }
 
-export const UserSelect: React.FC<IProps> = ({users, onUserSelect, onUserClear, filteredSubmissions}) => {
-	const [isGranted] = React.useContext(PermissionContext);
+export class UserSelect extends React.PureComponent<IProps, {}> {
+	public render(){
+		const {users, onUserClear, onUserSelect, selectedUser} = this.props;
 
-	if (!isGranted(Permission.ADMIN))
-		return null;
+		return (
+			<PermissionContext.Consumer>
+				{([isGranted]) => (
+					isGranted(Permission.ADMIN) && (
+						<div className="history-filter">
+							<Select<User>
+								items={users}
+								itemRenderer={this.renderUserOption}
+								onItemSelect={onUserSelect}
+								onClear={onUserClear}
+								noResults={(
+									<MenuItem
+										disabled={true}
+										text="No results."
+										roleStructure="listoption"
+									/>
+								)}
+							>
+								<Button
+									text={selectedUser ? `Show only ${selectedUser.firstName} ${selectedUser.lastName}` : 'Show All Users'}
+									rightIcon="caret-down"
+									alignText="left"
+									fill={true}
+								/>
+							</Select>
+						</div>
+					))}
+			</PermissionContext.Consumer>
+		)
+	}
 
-	return (
-		<div className="history-filter">
-			<span>Sort by</span>
+	private renderUserOption: ItemRenderer<User> = (user, {handleClick, handleFocus, modifiers}) => {
+		if (!modifiers.matchesPredicate)
+			return null;
 
-			<Select<User>
-				items={users}
-				noResults={<MenuItem disabled={true} text="No results." roleStructure="listoption" />}
-				itemRenderer={renderUserOption}
-				onItemSelect={onUserSelect}
-			>
-				<Button>
-					{filteredSubmissions && filteredSubmissions.length >= 1
-						? `${filteredSubmissions[0].user.name}`
-						: 'All Users'}
-				</Button>
-			</Select>
-
-			<Button minimal={true} small={true} onClick={onUserClear}>
-				Clear filter
-			</Button>
-		</div>
-	);
-};
-
-const renderUserOption: ItemRenderer<User> = (user, {handleClick, handleFocus, modifiers}) => {
-	if (!modifiers.matchesPredicate)
-		return null;
-
-	return (
-		<MenuItem
-			active={modifiers.active}
-			disabled={modifiers.disabled}
-			key={user.id}
-			onClick={handleClick}
-			onFocus={handleFocus}
-			roleStructure="listoption"
-			text={ucwords(`${user.firstName} ${user.lastName}`)}
-		/>
-	);
-};
+		return (
+			<MenuItem
+				active={modifiers.active}
+				disabled={modifiers.disabled}
+				selected={this.props.selectedUser === user}
+				key={user.id}
+				onClick={handleClick}
+				onFocus={handleFocus}
+				roleStructure="listoption"
+				text={ucwords(`${user.firstName} ${user.lastName}`)}
+			/>
+		);
+	};
+}
