8f457b66dbb9a8cb9929e61a6dd54c4b195e1436
Tyler Lartonoix <tyler@lartonoix.com>
2024-06-03 14:28:19 -0400

    Refactor to use new role / permission hooks

--- src/Components/NavHeader/SurveyMenu.tsx
+++ src/Components/NavHeader/SurveyMenu.tsx
@@ -1,27 +1,26 @@
 import {Menu, MenuDivider} from '@blueprintjs/core';
 import * as React from 'react';
-import {Permission, PermissionContext} from '../../Permission';
-import {Role, RoleContext} from '../../Role';
+import {Permission} from '../../Api/permissions';
+import {Role} from '../../Api/roles';
+import {withPermissionRestriction} from '../Router/withPermissionRestriction';
+import {withRoleRestriction} from '../Router/withRoleRestriction';
 import {LinkedMenuItem} from './LinkedMenuItem';
 
 export function SurveyMenu(): React.ReactElement {
-	const [isGranted] = React.useContext(PermissionContext);
-	const [isRoleGranted] = React.useContext(RoleContext);
-
 	return (
 		<Menu>
-			{isRoleGranted(Role.ADMIN) && (
+			{withRoleRestriction(Role.Admin, (
 				<>
 					<LinkedMenuItem to="/survey-bank" icon="projects" text="Survey Bank" />
 
 					<MenuDivider />
 				</>
-			)}
+			))}
 
 			<LinkedMenuItem to="/survey" icon="third-party" text="Take This Week's Survey" />
 			<LinkedMenuItem to="/results" icon="grouped-bar-chart" text="Last Week's Results" />
 
-			{isGranted(Permission.ADMIN) && (
+			{withPermissionRestriction(Permission.Admin, (
 				<>
 					<MenuDivider />
 
@@ -29,7 +28,7 @@ export function SurveyMenu(): React.ReactElement {
 					<LinkedMenuItem to="/survey/history" icon="history" text="Previous Survey Results" />
 					<LinkedMenuItem to="/survey/settings" icon="cog" text="Settings" />
 				</>
-			)}
+			))}
 		</Menu>
 	);
 }
