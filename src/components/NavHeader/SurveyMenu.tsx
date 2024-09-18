24d8de45dfac803c7d23364b506593c61323fdfd
Tyler Lartonoix <tyler@lartonoix.com>
2024-09-18 13:13:58 -0400

    WIP

--- src/components/NavHeader/SurveyMenu.tsx
+++ src/components/NavHeader/SurveyMenu.tsx
@@ -18,14 +18,14 @@ export function SurveyMenu(): React.ReactElement {
 			))}
 
 			<LinkedMenuItem to="/survey" icon="third-party" text="Take This Week's Survey" />
-			{/*<LinkedMenuItem to="/survey/results" icon="grouped-bar-chart" text="Last Week's Results" />*/}
+			<LinkedMenuItem to="/survey/results" icon="grouped-bar-chart" text="Last Week's Results" />
 
 			{withPermissionRestriction(Permission.Admin, (
 				<>
 					<MenuDivider />
 
 					<LinkedMenuItem to="/survey/next" icon="th-derived" text="Edit Next Week's Survey" />
-					{/*<LinkedMenuItem to="/survey/history" icon="history" text="Previous Survey Results" />*/}
+					<LinkedMenuItem to="/survey/history" icon="history" text="Previous Survey Results" />
 					<LinkedMenuItem to="/survey/settings" icon="cog" text="Settings" />
 				</>
 			))}
